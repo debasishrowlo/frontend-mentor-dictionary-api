cb7a1e12f5d9fdccbba99b08c07441e19a9a8a6e
debasishrowlo <debasishrowlo@gmail.com>
2023-10-08 12:55:18 +0530

    Refactor fonts

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,20 @@
-import React, { useEffect, useState, useRef, FormEvent, ReactNode } from "react"
+import { useEffect, useState, useRef, FormEvent } from "react"
 import { createRoot } from "react-dom/client"
-import classnames from "classnames"
 import { Listbox, Transition } from "@headlessui/react"
+import classnames from "classnames"
 
 import Logo from "./icons/Logo"
 import MoonIcon from "./icons/Moon"
 import PlayIcon from "./icons/Play"
 
 import arrowDownIcon from "./assets/images/icon-arrow-down.svg"
-import searchIcon from "./assets/images/icon-search.svg"
-import newWindowIcon from "./assets/images/icon-new-window.svg"
 import confusedEmojiIcon from "./assets/images/confused-emoji.svg"
+import newWindowIcon from "./assets/images/icon-new-window.svg"
+import searchIcon from "./assets/images/icon-search.svg"
 
 import "./index.css"
 
-type Word = {
+export type Word = {
   value: string,
   phonetic: string | null,
   audio: string | null,
@@ -29,7 +29,7 @@ type Word = {
   sourceUrls: string[],
 }
 
-type APIResponse = Array<{
+type Response = Array<{
   word: string,
   phonetic: string,
   phonetics: Array<{
@@ -71,28 +71,6 @@ const enum themes {
   dark = "dark",
 }
 
-const fontTypes:{ [key:string]: string } = {
-  sans: "sans",
-  serif: "serif",
-  mono: "mono",
-}
-
-const getFontName = (fontType:string) => {
-  if (fontType === fontTypes.serif) {
-    return "Serif"
-  }
-
-  if (fontType === fontTypes.sans) {
-    return "Sans Serif"
-  }
-
-  if (fontType === fontTypes.mono) {
-    return "Mono"
-  }
-
-  return null
-}
-
 const getDefaultTheme = () => {
   if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
     return themes.dark
@@ -101,18 +79,36 @@ const getDefaultTheme = () => {
   }
 }
 
+const fonts = [
+  {
+    name: "Serif",
+    className: "font-serif",
+  },
+  {
+    name: "Sans Serif",
+    className: "font-sans",
+  },
+  {
+    name: "Mono",
+    className: "font-mono",
+  },
+]
+
 const App = () => {
   const firstRenderRef = useRef(true)
 
   const [appState, setAppState] = useState<appStates>(appStates.empty)
-  const [query, setQuery] = useState("Keyboard")
-  const [font, setFont] = useState(fontTypes.serif)
-  const [theme, setTheme] = useState<themes>(getDefaultTheme())
-  const [word, setWord] = useState<Word|null>(null);
-  const [playButtonDisabled, setPlayButtonDisabled] = useState(false)
-  const [errorVisible, setErrorVisible] = useState(false)
 
-  const darkModeEnabled = theme === themes.dark
+  const [query, setQuery] = useState("Keyboard")
+  const [queryErrorVisible, setQueryErrorVisible] = useState(false)
+  const [activeFontIndex, setActiveFontIndex] = useState(0)
+
+  const [theme, setTheme] = useState<themes>(getDefaultTheme())
+  const [word, setWord] = useState<Word|null>(null)
+
+  const [playButtonDisabled, setPlayButtonDisabled] = useState(false)
+
+  const font = fonts[activeFontIndex]
 
   const search = async (word:string) => {
     setAppState(appStates.loading)
@@ -120,24 +116,17 @@ const App = () => {
     const response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
 
     if (!response.ok) {
-      if (response.status === 404) {
-        setAppState(appStates.notFound)
-        return
-      }
-
-      console.error("Something went wrong")
+      setAppState(appStates.notFound)
       return
     }
 
-    const responseJSON:APIResponse = await response.json()
+    const json:Response = await response.json()
 
-    const data = responseJSON[0]
+    const data = json[0]
 
     let partialWord:Partial<Word> = {
       value: data.word,
       sourceUrls: data.sourceUrls,
-      phonetic: null,
-      audio: null,
     }
 
     const phoneticWithAudio = data.phonetics.find((phonetic) => {
@@ -170,24 +159,8 @@ const App = () => {
       }
     })
 
-    const result:Word = partialWord as Word
-    setWord(result)
+    setWord(partialWord as Word)
     setAppState(appStates.word)
-  }
-
-  const handleSubmit = (e:FormEvent) => {
-    e.preventDefault()
-
-    if (query === "") {
-      setErrorVisible(true)
-      return
-    } 
-
-    if (errorVisible) {
-      setErrorVisible(false)
-    }
-
-    search(query)
   }
 
   const handleSynonymClick = (synonym:string) => {
@@ -196,35 +169,36 @@ const App = () => {
     window.scrollTo(0, 0)
   }
 
-  const getFontClassName = (fontType:string) => {
-    if (fontType === fontTypes.serif) {
-      return "font-serif"
-    }
-
-    if (fontType === fontTypes.sans) {
-      return "font-sans"
-    }
-
-    if (fontType === fontTypes.mono) {
-      return "font-mono"
-    }
-
-    return null
-  }
-
   const toggleTheme = () => {
     setTheme(theme === themes.dark ? themes.light : themes.dark)
   }
 
   const playAudio = ()  => {
-    if (word.audio !== null) {
-      setPlayButtonDisabled(true)
-      const audio = new Audio(word.audio)
-
-      audio.play()
-
-      audio.onended = () => setPlayButtonDisabled(false)
+    if (word.audio == null) {
+      return
     }
+
+    setPlayButtonDisabled(true)
+    const audio = new Audio(word.audio)
+
+    audio.play()
+
+    audio.onended = () => setPlayButtonDisabled(false)
+  }
+
+  const handleSubmit = (e:FormEvent) => {
+    e.preventDefault()
+
+    if (query === "") {
+      setQueryErrorVisible(true)
+      return
+    } 
+
+    if (queryErrorVisible) {
+      setQueryErrorVisible(false)
+    }
+
+    search(query)
   }
 
   useEffect(() => {
@@ -247,14 +221,14 @@ const App = () => {
   }, [theme])
 
   return (
-    <div className={`p-6 max-w-[736px] mx-auto md:px-9 md:py-14 lg:px-0 ${getFontClassName(font)}`}>
+    <div className={`p-6 max-w-[736px] mx-auto md:px-9 md:py-14 lg:px-0 ${font.className}`}>
       <div className="flex justify-between">
         <Logo className="w-6 stroke-current text-gray-300 md:w-8" />
         <div className="flex items-center">
           <div className="h-full relative z-10">
-            <Listbox value={font} onChange={setFont}>
+            <Listbox value={activeFontIndex} onChange={(index:number) => setActiveFontIndex(index)}>
               <Listbox.Button className="h-full px-4 flex items-center">
-                <span className="text-14 font-bold dark:text-white md:text-18">{getFontName(font)}</span>
+                <span className="text-14 font-bold dark:text-white md:text-18">{font.name}</span>
                 <img src={arrowDownIcon} className="w-3 ml-4" />
               </Listbox.Button>
               <Transition
@@ -266,13 +240,13 @@ const App = () => {
                 leaveTo="-translate-y-2 opacity-0"
               >
                 <Listbox.Options className="w-44 mt-2 py-4 absolute z-10 top-full right-4 bg-white dark:bg-gray-600 rounded-2xl shadow-[0_5px_30px_0px_rgba(0,0,0,0.10)] dark:shadow-purple">
-                  {Object.keys(fontTypes).map((fontKey) => (
+                  {fonts.map((font, index) => (
                     <Listbox.Option 
-                      key={fontKey}
-                      value={fontKey}
-                      className={`${getFontClassName(fontKey)} px-6 py-2 text-14 font-bold dark:text-white hover:text-purple whitespace-nowrap cursor-pointer transition-colors duration-200 md:text-18`}
+                      key={index}
+                      value={index}
+                      className={`${font.className} px-6 py-2 text-14 font-bold dark:text-white hover:text-purple whitespace-nowrap cursor-pointer transition-colors duration-200 md:text-18`}
                     >
-                      {getFontName(fontKey)}
+                      {font.name}
                     </Listbox.Option>
                   ))}
                 </Listbox.Options>
@@ -286,10 +260,7 @@ const App = () => {
           >
             <div className="w-10 h-5 px-1 flex items-center bg-gray-300 hover:bg-purple dark:bg-purple rounded-full duration-300 transition-colors">
               <div className="w-full h-full relative">
-                <div className={classnames("w-3.5 h-3.5 absolute top-1/2 -translate-y-1/2 bg-white rounded-full transition-all duration-500", {
-                  "left-0 translate-x-0": !darkModeEnabled,
-                  "left-full -translate-x-full": darkModeEnabled,
-                })}></div>
+                <div className="w-3.5 h-3.5 absolute top-1/2 -translate-y-1/2 bg-white rounded-full transition-all duration-500 left-0 translate-x-0 dark:left-full dark:-translate-x-full"></div>
               </div>
             </div>
             <MoonIcon className="ml-3 w-5 stroke-current text-gray-300 dark:text-purple" />
@@ -301,8 +272,8 @@ const App = () => {
           <input 
             type="text" 
             className={classnames("w-full pl-6 pr-14 py-3 border bg-transparent font-bold dark:text-white rounded-2xl outline-none caret-purple transition-colors duration-200 md:text-20", {
-              "border-transparent focus:border-purple ": !errorVisible,
-              "border-red": errorVisible,
+              "border-transparent focus:border-purple ": !queryErrorVisible,
+              "border-red": queryErrorVisible,
             })}
             value={query}
             onChange={(e) => setQuery(e.target.value)}
@@ -313,8 +284,15 @@ const App = () => {
           </button>
         </form>
       </div>
-      {errorVisible && (
+      {queryErrorVisible && (
         <p className="mt-2 text-16 md:text-18 lg:text-20 text-red">Whoops, can’t be empty…</p>
+      )}
+      {(appState === appStates.notFound) && (
+        <div className="mt-[132px]">
+          <img src={confusedEmojiIcon} className="w-10 mx-auto md:w-14 lg:w-16" />
+          <p className="mt-6 text-center font-bold text-16 dark:text-white md:mt-8 md:text-20 lg:mt-11">No definitions found</p>
+          <p className="mt-3 text-center text-gray-300 text-14 md:mt-4 md:text-18 lg:mt-6">Sorry pal, we couldn't find definitions for the word you were looking for. You can try the search again at later time or head to the web instead.</p>
+        </div>
       )}
       {(appState === appStates.word) && (
         <>
@@ -341,7 +319,7 @@ const App = () => {
             <div key={index} className="mt-7 md:mt-10">
               <div className="flex items-center">
                 <p className={classnames("text-18 font-bold dark:text-white md:text-24", {
-                  "italic": font === fontTypes.serif
+                  "italic": font.name === "Serif",
                 })}>{meaning.partOfSpeech}</p>
                 <div className="ml-4 grow border-t border-gray-200 dark:border-gray-400" />
               </div>
@@ -394,15 +372,8 @@ const App = () => {
           )}
         </>
       )}
-      {(appState === appStates.notFound) && (
-        <div className="mt-[132px]">
-          <img src={confusedEmojiIcon} className="w-10 mx-auto md:w-14 lg:w-16" />
-          <p className="mt-6 text-center font-bold text-16 dark:text-white md:mt-8 md:text-20 lg:mt-11">No definitions found</p>
-          <p className="mt-3 text-center text-gray-300 text-14 md:mt-4 md:text-18 lg:mt-6">Sorry pal, we couldn't find definitions for the word you were looking for. You can try the search again at later time or head to the web instead.</p>
-        </div>
-      )}
     </div>
-  );
+  )
 }
 
 const container = document.getElementById("app")
